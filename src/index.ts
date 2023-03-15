4f66fd944835f68f06cb897e915d21f213752a9d
skwee357 <ko.dmitri@gmail.com>
2023-03-15 19:09:56 +0200

    fix es module interop

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { CollectionInterface } from './collection';
 import { Brand, Manufacturer, Switch } from './switch';
 import collection from './switches';
 import { MechanicalKeySwitch } from './collection';
-import pkg from '../package.json';
+import * as pkg from '../package.json';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
 const VERSION = pkg.version;
